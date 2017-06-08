57deedc405e2b724d604b916521f71dddbb0ce10
manishkjain21 <manishkjain21@gmail.com>
2017-06-08 16:28:20 +0200

    Updated the code for Kernel thread

--- .tmp_versions/kernel_panda.mod
+++
@@ -1,2 +0,0 @@
-/home/seceng/Desktop/Manish/Kernel_Modules/kernel_panda.ko
-/home/seceng/Desktop/Manish/Kernel_Modules/kernel_panda.o
--- kernel_thread.mod.c
+++ kernel_thread.mod.c
@@ -19,10 +19,11 @@ __used
 __attribute__((section("__versions"))) = {
 	{ 0x62c440a7, "module_layout" },
 	{ 0xa4f3dfa0, "wake_up_process" },
+	{ 0x37a0cba, "kfree" },
+	{ 0xc9a008df, "kthread_stop" },
 	{ 0x2dc7be74, "kthread_create_on_node" },
 	{ 0xefd6cf06, "__aeabi_unwind_cpp_pr0" },
 	{ 0x7d11c268, "jiffies" },
-	{ 0xc9a008df, "kthread_stop" },
 	{ 0x1000e51, "schedule" },
 	{ 0x27e1a049, "printk" },
 };
@@ -33,4 +34,4 @@ __attribute__((section(".modinfo"))) =
 "depends=";
 
 
-MODULE_INFO(srcversion, "11DB14219AA2867B7E23929");
+MODULE_INFO(srcversion, "ED4CB830D2518C1010551E7");
